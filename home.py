623de82c80f71a6d1f9fe9853b15b25f0d02c81b
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2021-04-24 21:49:34 -0500

    Cambios.

--- home.py
+++ home.py
@@ -6,5 +6,41 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/ListaEnlazada')
+def listase():
+    return render_template('code/listase.html')
+
+@app.route('/ListaDobleEnlazada')
+def listade():
+    return render_template('code/listade.html')
+
+@app.route('/ListaCircularSimple')
+def listaC():
+    return render_template('code/circularS.html')
+
+@app.route('/ListaDobleCircularEnlazada')
+def listaCD():
+    return render_template('code/circularD.html')
+
+@app.route('/ArbolAVL')
+def arbolAVL():
+    return render_template('code/arbolAVL.html')
+
+@app.route('/Pilas')
+def pila():
+    return render_template('code/pilaA.html')
+
+@app.route('/Colas')
+def cola():
+    return render_template('code/colas.html')
+
+
+@app.route('/ArbolBinario')
+def binario():
+    return render_template('code/arbolbi.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
