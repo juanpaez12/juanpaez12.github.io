e5a6dacd81860b79f1f2fdaec7a5abbbaf2fdb9e
juanpaez12 <61242333+juanpaez12@users.noreply.github.com>
2021-04-24 19:31:46 -0500

    Update home.py

---
+++ home.py
@@ -0,0 +1,10 @@
+from flask import flask, render_template
+
+app = flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
