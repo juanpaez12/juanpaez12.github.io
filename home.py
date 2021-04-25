f66bf99ab1fbf72cfc1c8121f466abd03231dfbd
juanpaez12 <61242333+juanpaez12@users.noreply.github.com>
2021-04-24 23:27:42 -0500

    a

--- home.py
+++ home.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+@app.route('/home')
+def index():
     return render_template('index.html')
 
 @app.route('/ListaEnlazada')
